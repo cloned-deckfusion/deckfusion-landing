d9e07c924799751b4da8d81734ce6588136940e4
Sean O'Leary <seamicole@gmail.com>
2024-11-16 06:43:52 -1000

    Update confetti effect

--- static/js/confetti.js
+++ static/js/confetti.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('.early-access-form');
-    if (form) {
+    const message = document.createElement('p');
+    message.textContent = "Hang tight! We'll be in touch soon.";
+    message.className = 'early-access-message hero-gradient hidden';
+
+    if (form && form.parentElement) {
+        form.parentElement.appendChild(message);
+
         form.addEventListener('submit', function (event) {
             event.preventDefault();
 
@@ -9,10 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 spread: 70,
                 origin: { y: 0.6 }
             });
+
             form.classList.add('fade-out');
 
             setTimeout(() => {
                 form.classList.add('hidden');
+                message.classList.remove('hidden');
+                message.classList.add('fade-in');
             }, 500);
         });
     }
